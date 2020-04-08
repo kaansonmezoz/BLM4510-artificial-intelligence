df752e224e4cc72e81a3c520d2fd99c7780e9512
Kaan <ksonmezoz@outlook.com>
2020-04-08 04:31:58 +0300

    Added node which is basically a pixel in image

---
+++ ödev-1/stack-based-search/node.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, x, y, red, current_to_goal_cost):
+        self.x = x
+        self.y = y
+        self.red = red
+        self.source_to_current_cost = 0
+        self.current_to_goal_cost = current_to_goal_cost
+
+    @classmethod
+    def from_image(cls, image, row, column, goal_x, goal_y):        
+        image_nodes = [[0 for y in range(column)] for x in range(row)]
+        
+        goal_red = image[goal_x][goal_y][0]
+        current_to_goal_cost = 255 - goal_red
+
+        for i in range(row):        
+            for j in range(column):            
+                red = image[i][j][0]
+                image_nodes[i][j] = cls(i, j, red, current_to_goal_cost)
+    
+        return image_nodes
+
+    def set_source_to_current_cost(self, source_to_current_cost):
+        self.source_to_current_cost = source_to_current_cost
+    
+    def set_total_cost(self, total_cost):
+        self.total_cost = total_cost    
+        
+    def update(self, source_to_current_cost, current_to_goal_cost):
+        self.source_to_current_cost = source_to_current_cost
+        self.current_to_goal_cost = current_to_goal_cost
+    
+    def x(self):
+        return self.x
+    
+    def y(self):
+        return self.y
+    
+    def red(self):
+        return self.red
+    
+    def source_to_current_cost(self):
+        return self.source_to_current_cost
+    
+    def current_to_goal_cost(self):
+        return self.current_to_goal_cost
+    
+    def total_cost(self):
+        return self.current_to_goal_cost + self.source_to_current_cost
