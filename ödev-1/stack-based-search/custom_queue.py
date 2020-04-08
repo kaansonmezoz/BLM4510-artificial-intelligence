5ff14bea129f7684e2708f41dc2226a0693588be
Kaan <ksonmezoz@outlook.com>
2020-04-08 05:07:08 +0300

    Implemented sort() and deleted comments

--- ödev-1/stack-based-search/custom_queue.py
+++ ödev-1/stack-based-search/custom_queue.py
@@ -1,25 +1,31 @@
+from functools import cmp_to_key
+
 class Priority_Queue:
     def __init__(self):
         self.queue = []
         self.items = set()
     
     def add_node(self, node):
-        ## burada node'un eklenip eklenmediği de kontrol edilmeli ayrıca...
-        ## queue'da bu eleman varsa gerekli işlemler yapılmalı ne gibi ?
-        ## cost'u eğer aynı elemanın diğer versiyonundan daha az ise costun değiştirilmesi gerekir.
-        ## sonrasında da tekrardan sıralanması
         self.queue.append(node)
         self.items.add(node)
+        self.sort()
         
     def update_node(self, node, source_to_current_cost):
         node.set_source_to_current_cost(source_to_current_cost)
         self.sort()
     
-    ## total_cost'a göre sıralanmalı burası. total_cost'lar eşitse goal'a daha yakın olanı öne koy.
-    ## yani current_to_goal_cost
     def sort(self): 
-        return 
-
+        self.queue.sort(key = cmp_to_key(self.compare))
+    
+    def compare(self, first_node, second_node):
+        difference = first_node.total_cost() - second_node.total_cost()
+        
+        if difference != 0:
+            return difference
+        
+        # hangisinin goal'e gitmesi daha az maliyetli ise o önce gelir.
+        return first_node.current_to_goal_cost() - second_node.current_to_goal_cost() 
+        
     def is_empty(self):
         return len(self.queue) == 0
     
